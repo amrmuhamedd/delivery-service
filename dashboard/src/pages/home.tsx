2b99ac636c1be86bf529281f06934038296cf544
Amr Mohamed <amrmuhmedd@gmail.com>
2023-02-15 02:08:11 +0200

    add logout button

--- dashboard/src/pages/home.tsx
+++ dashboard/src/pages/home.tsx
@@ -1,10 +1,19 @@
+import { Button } from 'antd'
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import ParcelTable from '../components/parcelsTable/parcels-table'
 import Logo from '../components/ui/logo/logo'
+import { remove } from '../redux/reducers/userSlice'
 
 function Home() {
+  const dispatch = useDispatch()
+  const handleLogout = () => {
+    localStorage.clear()
+    dispatch(remove())
+  }
   return (
     <div className='home-page'>
+      <Button className='logout-btn' type='primary' onClick={handleLogout}>Logout</Button>
       <Logo />
       <ParcelTable />
     </div>
