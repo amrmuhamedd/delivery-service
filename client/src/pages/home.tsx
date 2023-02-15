2ba77b0332f6f0509252b05f61669cd3d69b096e
Amr Mohamed <amrmuhmedd@gmail.com>
2023-02-15 16:24:49 +0200

    add username

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,19 +1,22 @@
 import { Button } from 'antd'
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import ParcelTable from '../components/parcelsTable/parcels-table'
 import Logo from '../components/ui/logo/logo'
 import { remove } from '../redux/reducers/userSlice'
+import { RootState } from '../redux/store'
 
 function Home() {
   const dispatch = useDispatch()
+  const user = useSelector((state: RootState) => state.user);
+
   const handleLogout = () => {
     localStorage.clear()
     dispatch(remove())
   }
   return (
     <div className='home-page'>
-      <Button className='logout-btn' type='primary' onClick={handleLogout}>Logout</Button>
+      <div className='logout-btn'><span className='name'>{user.username}</span>  <Button  type='primary' onClick={handleLogout}>Logout</Button></div>
       <Logo />
       <ParcelTable />
     </div>
